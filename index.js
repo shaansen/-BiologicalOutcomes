643d5047ffcf8f9bb8508d3bebe52dfd5bdef00e
Shantanu <shantanu.sengupta2008@gmail.com>
2017-10-04 14:32:20 -0400

    Parallel Coordinates

--- index.js
+++ index.js
@@ -1,98 +1,124 @@
-var width = 960,
-    size = 230,
-    padding = 20;
+var margin = {top: 30, right: 10, bottom: 10, left: 10},
+    width = 1920 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom;
 
-var x = d3.scale.linear()
-    .range([padding / 2, size - padding / 2]);
+var x = d3.scale.ordinal().rangePoints([0, width], 1),
+    y = {};
 
-var y = d3.scale.linear()
-    .range([size - padding / 2, padding / 2]);
+var line = d3.svg.line(),
+    axis = d3.svg.axis().orient("left"),
+    background,
+    foreground;
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-    .ticks(6);
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(6);
+d3.selectAll("#TreatmentType").on("change",update);
+      update();
 
-var color = d3.scale.category10();
+d3.selectAll("#TissueType").on("change",update);
+      update();
 
-d3.csv("data/data2016.csv", function(error, data) {
-  if (error) throw error;
 
-  var domainByTrait = {},
-      traits = d3.keys(data[0]).filter(function(d) { return d!=="mouse_sample"||d!=="Tissue_Type"||d!=="Treatment"; }),
-      n = traits.length;
+function update(){
 
-  traits.forEach(function(trait) {
-    domainByTrait[trait] = d3.extent(data, function(d) { return d[trait]; });
+  console.log("Updating this page again and again")
+  var treatmentChoices = [];
+  var tissueChoices = [];
+  
+  d3.selectAll("#TreatmentType").each(function(d){
+    cb = d3.select(this);
+    if(cb.property("checked")){
+      treatmentChoices.push(cb.property("value"));
+    }
   });
 
-  xAxis.tickSize(size * n);
-  yAxis.tickSize(-size * n);
+  d3.selectAll("#TissueType").each(function(d){
+    cb = d3.select(this);
+    if(cb.property("checked")){
+      tissueChoices.push(cb.property("value"));
+    }
+  });
+  
+  d3.csv("data/data2016.csv", function(error, mfi) { 
+    
+    if(treatmentChoices.length !== 0 || tissueChoices.length !== 0) {
+      let sampleArray = mfi.filter(d => {
+        //console.log(treatmentChoices.includes(d["Treatment"]));
+        return treatmentChoices.includes(d["Treatment"]) || tissueChoices.includes(d["Tissue_Type"])
+      })
+      mfi = sampleArray
+    } else {
+      mfi = []
+    }
+    
+    // Extract the list of dimensions and create a scale for each.
+    x.domain(dimensions = d3.keys(mfi[0]).filter(function(d) {
+      return (d != "mouse_sample" && d != "Tissue_Type" && d != "Treatment") && (y[d] = d3.scale.linear()
+          .domain(d3.extent(mfi, function(p) { return +p[d]; }))
+          .range([height, 0]));
+    }));
 
-  var svg = d3.select("body").append("svg")
-      .attr("width", size * n + padding)
-      .attr("height", size * n + padding)
-    .append("g")
-      .attr("transform", "translate(" + padding + "," + padding / 2 + ")");
+    // Add grey background lines for context.
+    background = svg.append("g")
+      .attr("class", "background")
+      .selectAll("path")
+      .data(mfi)
+      .enter().append("path")
+      .attr("d", path);
 
-  svg.selectAll(".x.axis")
-      .data(traits)
-    .enter().append("g")
-      .attr("class", "x axis")
-      .attr("transform", function(d, i) { return "translate(" + (n - i - 1) * size + ",0)"; })
-      .each(function(d) { x.domain(domainByTrait[d]); d3.select(this).call(xAxis); });
+    // Add blue foreground lines for focus.
+    foreground = svg.append("g")
+      .attr("class", "foreground")
+      .selectAll("path")
+      .data(mfi)
+      .enter().append("path")
+      .attr("d", path)
+      .attr("stroke", function(d,i) {
+        return "rgb("+(i*5)%255+","+(i*2)%255+","+(i*4)%255+")";
+      })
 
-  svg.selectAll(".y.axis")
-      .data(traits)
-    .enter().append("g")
-      .attr("class", "y axis")
-      .attr("transform", function(d, i) { return "translate(0," + i * size + ")"; })
-      .each(function(d) { y.domain(domainByTrait[d]); d3.select(this).call(yAxis); });
+    // Add a group element for each dimension.
+    var g = svg.selectAll(".dimension")
+      .data(dimensions)
+      .enter().append("g")
+      .attr("class", "dimension")
+      .attr("transform", function(d) { return "translate(" + x(d) + ")"; });
 
-  var cell = svg.selectAll(".cell")
-      .data(cross(traits, traits))
-    .enter().append("g")
-      .attr("class", "cell")
-      .attr("transform", function(d) { return "translate(" + (n - d.i - 1) * size + "," + d.j * size + ")"; })
-      .each(plot);
+    // Add an axis and title.
+    g.append("g")
+      .attr("class", "axis")
+      .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
+      .append("text")
+      .style("text-anchor", "middle")
+      .attr("y", -9)
+      .text(function(d) { return d; });
 
-  // Titles for the diagonal.
-  cell.filter(function(d) { return d.i === d.j; }).append("text")
-      .attr("x", padding)
-      .attr("y", padding)
-      .attr("dy", ".71em")
-      .text(function(d) { return d.x; });
+    // Add and store a brush for each axis.
+    g.append("g")
+      .attr("class", "brush")
+      .each(function(d) { d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brush", brush)); })
+      .selectAll("rect")
+      .attr("x", -8)
+      .attr("width", 16);
+  });
+}
 
-  function plot(p) {
-    var cell = d3.select(this);
+// Returns the path for a given data point.
+function path(d) {
+  return line(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
+}
 
-    x.domain(domainByTrait[p.x]);
-    y.domain(domainByTrait[p.y]);
-
-    cell.append("rect")
-        .attr("class", "frame")
-        .attr("x", padding / 2)
-        .attr("y", padding / 2)
-        .attr("width", size - padding)
-        .attr("height", size - padding);
-
-    cell.selectAll("circle")
-        .data(data)
-      .enter().append("circle")
-        .attr("cx", function(d) { return x(d[p.x]); })
-        .attr("cy", function(d) { return y(d[p.y]); })
-        .attr("r", 4)
-        .style("fill", function(d) { return color(d.Treatment); });
-  }
-});
-
-function cross(a, b) {
-  var c = [], n = a.length, m = b.length, i, j;
-  for (i = -1; ++i < n;) for (j = -1; ++j < m;) c.push({x: a[i], i: i, y: b[j], j: j});
-  return c;
+// Handles a brush event, toggling the display of foreground lines.
+function brush() {
+  var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
+      extents = actives.map(function(p) { return y[p].brush.extent(); });
+  foreground.style("display", function(d) {
+    return actives.every(function(p, i) {
+      return extents[i][0] <= d[p] && d[p] <= extents[i][1];
+    }) ? null : "none";
+  });
 }
