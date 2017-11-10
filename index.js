9894d887a64689da6f70f2e3166d3c158c205145
shojo1 <shojo1@umbc.edu>
2017-11-10 17:41:25 -0500

    can upload and change data sources

---
+++ index.js
@@ -0,0 +1,206 @@
+function upload() {
+    fetch('/submit', {
+        method: 'post',
+        body: new FormData(document.querySelector('#form'))
+    }).then(function json(response) {
+        return response.json()
+    }).then(function(data) {
+        window.console.log('Request succeeded with JSON response', data);
+    }).catch(function(error) {
+        window.console.log('Request failed', error);
+    });
+}
+
+
+
+$( document ).ready(function() {
+  $.ajax({
+    url : "/data",
+    dataType: "html",
+    success : function(result){
+
+        $(".result").html(result);
+        id = $(".result").find('.ntable');
+
+        var row = [];
+
+        $(".ntable a").each(function() {
+            var substring = ".csv";
+            if($(this).attr('href').indexOf(substring) !== -1){
+              row.push($(this).attr('href'));
+            }
+        });
+        // console.log("row");
+        console.log(row);
+        var dropdown = $('#dataSources');
+        dropdown.options = function (data) {
+            var self = this;
+            $.each(data, function (ix, val) {
+                var option = $('<option>').text(val);
+                data.push(option);
+            });
+            self.append(data)
+        }
+        dropdown.empty();
+        dropdown.options(row);
+    }
+  });
+
+  var margin = {top: 30, right: 10, bottom: 10, left: 10},
+      width = 1920 - margin.left - margin.right,
+      height = 700 - margin.top - margin.bottom;
+
+  var x = d3.scale.ordinal().rangePoints([0, width], 1),
+      y = {};
+
+  var line = d3.svg.line(),
+      axis = d3.svg.axis().orient("left"),
+      background,
+      foreground;
+
+  var svg = d3.select(".viz").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var data = "data/data2016.csv";//var data = "data/" + data2016.csv;
+
+  d3.selectAll("#TreatmentType").on("change",wasClicked);
+        wasClicked();
+
+  d3.selectAll("#TissueType").on("change",wasClicked);
+        wasClicked();
+        
+  d3.selectAll("#dataSources").on("change",wasClicked);
+        wasClicked();
+
+  function wasClicked(){
+    var dropdown = $('#dataSources option:selected').text();
+    var data1 = "data/" + dropdown;
+    update(data1);
+  }
+
+  function update(dataString){
+    console.log("dataString");
+    console.log(dataString);
+    var treatmentChoices = [];
+    var tissueChoices = [];
+    let cb = []
+
+    d3.selectAll("#TreatmentType").each(function(d){
+      cb = d3.select(this);
+      if(cb.property("checked")){
+        treatmentChoices.push(cb.property("value"));
+      } else {
+      }
+    });
+
+    d3.selectAll("#TissueType").each(function(d){
+      cb = d3.select(this);
+      if(cb.property("checked")){
+        tissueChoices.push(cb.property("value"));
+      } else {
+      }
+    });
+
+    d3.csv(dataString, function(error, mfi) {
+
+      if(treatmentChoices.length !== 0 || tissueChoices.length !== 0) {
+        let sampleArray = mfi.filter(d => {
+          return treatmentChoices.includes(d["Treatment"]) && tissueChoices.includes(d["Tissue_Type"])
+        })
+        mfi = sampleArray
+      } else {
+        mfi = []
+      }
+
+
+
+      let avoidColumns = ["index","mouse_sample","Tissue_Type","Treatment"]
+      x.domain(dimensions = d3.keys(mfi[0]).filter(function(d) {
+        return (!avoidColumns.includes(d)) && (y[d] = d3.scale.linear()
+            .domain(d3.extent(mfi, function(p) { return +p[d]; }))
+            .range([height, 0]));
+      }));
+
+      svg.selectAll("g").remove();
+
+      background = svg.append("g")
+        .attr("class", "background")
+        .selectAll("path")
+        .data(mfi)
+        .enter().append("path")
+        .attr("d", path);
+
+      foreground = svg.append("g")
+        .attr("class", "foreground")
+        .selectAll("path")
+        .data(mfi)
+
+      foreground.exit().remove()
+
+      foreground.enter().append("path")
+        .attr("d", path)
+        .attr("stroke", function(d) {
+          let i = d["index"]
+          console.log(d["Treatment"])
+          if(d["Treatment"]==="AIP") {
+            return "rgb(255,0,0)";
+          } else if(d["Treatment"]==="AIPV") {
+            return "rgb(0,255,0)";
+          } else if(d["Treatment"]==="AIV") {
+            return "rgb(0,0,255)";
+          } else if(d["Treatment"]==="APV") {
+            return "rgb(120,120,0)";
+          } else if(d["Treatment"]==="IPV") {
+            return "rgb(0,120,120)";
+          } else {
+            return "rgb(120,0,120)";
+          }
+
+        })
+
+      var g = svg.selectAll(".dimension")
+        .data(dimensions)
+        .enter().append("g")
+        .attr("class", "dimension")
+        .attr("transform", function(d) { return "translate(" + x(d) + ")"; });
+
+      // Add an axis and title.
+      g.append("g")
+        .attr("class", "axis")
+        .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
+        .append("text")
+        .style("text-anchor", "middle")
+        .attr("y", -9)
+        .text(function(d) { return d; });
+
+      // Add and store a brush for each axis.
+      g.append("g")
+        .attr("class", "brush")
+        .each(function(d) { d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brush", brush)); })
+        .selectAll("rect")
+        .attr("x", -8)
+        .attr("width", 16);
+
+    });
+
+  }
+
+  // Returns the path for a given data point.
+  function path(d) {
+    return line(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
+  }
+
+  // Handles a brush event, toggling the display of foreground lines.
+  function brush() {
+    var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
+        extents = actives.map(function(p) { return y[p].brush.extent(); });
+    foreground.style("display", function(d) {
+      return actives.every(function(p, i) {
+        return extents[i][0] <= d[p] && d[p] <= extents[i][1];
+      }) ? null : "none";
+    });
+  }
+});
