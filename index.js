daac55b10ca7be74c87c6ac20158f621e179bcf1
Shantanu <shantanu.sengupta2008@gmail.com>
2017-12-05 11:36:38 -0500

    Accordion Format for Control Menu

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+// Code to toggle Active Accordions
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
+
 //D3 Variables Section
+
 var width = document.body.clientWidth-300,
   height = d3.max([document.body.clientHeight-20, 240]);
 
